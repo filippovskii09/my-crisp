1aeb3b15533233c084dcb3a5649ff191dd7c4f5a
filippovskiy09 <arturfilippovskij@gmail.com>
2023-11-16 13:10:41 +0200

    first

---
+++ js/index.js
@@ -0,0 +1,56 @@
+window.addEventListener('DOMContentLoaded', () => {
+	const cartItems = document.querySelector('.header-cart__items'),
+		cartTotalPrice = document.querySelector('.header-cart__price'),
+		closeLogin = document.querySelector('.sign__close'),
+		openLogin = document.querySelector('.header-info__sign'),
+		signModal = document.querySelector('.sign'),
+		cartMOdalTotalPrice = document.querySelector('#cart-total'),
+		openCart = document.querySelector('.header-cart__btn'),
+		closeCart = document.querySelector('.cart__close'),
+		cartModal = document.querySelector('.cart'),
+		header = document.querySelector('.header');
+
+
+	cartItems.textContent = 'Shopping Cart';
+	cartTotalPrice.textContent = '0,00 EUR';
+	cartMOdalTotalPrice.textContent = '139,00 EUR';
+
+	openLogin.addEventListener('click', () => {
+		signModal.classList.add('sign-open')
+		console.log('sign open');
+	})
+
+	closeLogin.addEventListener('click', () => {
+		signModal.classList.remove('sign-open')
+	})
+
+	openCart.addEventListener('click', () => {
+		cartModal.classList.add('cart-open')
+	})
+
+	closeCart.addEventListener('click', () => {
+		cartModal.classList.remove('cart-open')
+	})
+
+	window.addEventListener('scroll', () => {
+		let scrollPos = window.scrollY
+
+		if (scrollPos >= 20) {
+			header.classList.add('header-fixed')
+		} else {
+			header.classList.remove('header-fixed')
+		}
+	})
+
+
+	// burger button
+
+	const burgerButton = document.querySelector('.header__burger');
+
+	burgerButton.addEventListener('click', () => {
+		burgerButton.classList.toggle('open');
+		document.documentElement.classList.toggle('menu-open');
+	})
+
+	// adaptive 
+})
