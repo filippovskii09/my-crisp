f34fdb8745463121a969708e76a30182a551d891
filippovskiy09 <arturfilippovskij@gmail.com>
2023-11-18 01:12:10 +0200

    add new block

--- js/index.js
+++ js/index.js
@@ -32,15 +32,17 @@ window.addEventListener('DOMContentLoaded', () => {
 		cartModal.classList.remove('cart-open')
 	})
 
-	window.addEventListener('scroll', () => {
-		let scrollPos = window.scrollY
+	// header add class on scroll
 
-		if (scrollPos >= 20) {
-			header.classList.add('header-fixed')
-		} else {
-			header.classList.remove('header-fixed')
-		}
-	})
+	// window.addEventListener('scroll', () => {
+	// 	let scrollPos = window.scrollY;
+
+	// 	if (scrollPos >= 20) {
+	// 		header.classList.add('header-fixed')
+	// 	} else {
+	// 		header.classList.remove('header-fixed')
+	// 	}
+	// })
 
 
 	// burger button
@@ -52,5 +54,116 @@ window.addEventListener('DOMContentLoaded', () => {
 		document.documentElement.classList.toggle('menu-open');
 	})
 
-	// adaptive 
+	// scroll on button
+
+	const prevScroll = document.querySelectorAll('#prev-scroll'),
+			nextScroll = document.querySelectorAll('#next-scroll'),
+			newProducts = document.querySelector('#new-products'),
+			trendingProducts = document.querySelector('#trending');
+
+	prevScroll[0].addEventListener('click', () => {
+		newProducts.scrollTo({
+			left: newProducts.scrollLeft - 600,
+			behavior: 'smooth'
+		})
+	})
+
+	nextScroll[0].addEventListener('click', () => {
+		newProducts.scrollTo({
+			left: newProducts.scrollLeft + 600,
+			behavior: 'smooth'
+		})
+	})
+
+	prevScroll[1].addEventListener('click', () => {
+		trendingProducts.scrollTo({
+			left: trendingProducts.scrollLeft - 600,
+			behavior: 'smooth'
+		})
+	})
+
+	nextScroll[1].addEventListener('click', () => {
+		trendingProducts.scrollTo({
+			left: trendingProducts.scrollLeft + 600,
+			behavior: 'smooth'
+		})
+	})
+
+	// get new-products 
+
+	async function getNewProducts(url) {
+		try {
+			const response = await fetch(url);
+			const data = await response.json();
+			const ul = document.querySelector('#new-products');
+
+			function displayProducts() {
+				ul.innerHTML = '';
+
+				const filteredData = data.filter(item => item.category === 'NEW Arivals');
+
+				filteredData.forEach(item => {
+					const li = document.createElement('li');
+					li.classList.add('products__item');
+					li.innerHTML = `
+							 <a href="./details.html?id=${item.id}">
+								  <div class="item-products__image">
+										<img src="${item.image}" alt="image">
+								  </div>
+								  <h5 class="item-products__category">${item.category}</h5>
+								  <p class="item-products__name">${item.name}</p>
+								  <h4 class="item-products__price">${item.price}</h4>
+							 </a>
+						`;
+					ul.appendChild(li);
+				});
+			}
+
+			displayProducts();
+
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	getNewProducts('data.json');
+
+
+	async function getTrendingProducts(url) {
+		try {
+			const response = await fetch(url);
+			const data = await response.json();
+			const ul = document.querySelector('#trending');
+
+			function displayProducts() {
+				ul.innerHTML = '';
+
+				const filteredData = data.filter(item => item.category === 'TREnding');
+
+				filteredData.forEach(item => {
+					const li = document.createElement('li');
+					li.classList.add('products__item');
+					li.innerHTML = `
+							 <a href="./details.html?id=${item.id}">
+								  <div class="item-products__image">
+										<img src="${item.image}" alt="image">
+								  </div>
+								  <h5 class="item-products__category">${item.category}</h5>
+								  <p class="item-products__name">${item.name}</p>
+								  <h4 class="item-products__price">${item.price}</h4>
+							 </a>
+						`;
+					ul.appendChild(li);
+				});
+			}
+
+			displayProducts();
+
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	getTrendingProducts('data.json');
+
 })
